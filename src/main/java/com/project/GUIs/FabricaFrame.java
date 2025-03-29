771e638253dc02b4ee627aadc9a595c747d46f36
Camilo Alejandro Linares Moreno <39498026+VaggerGG@users.noreply.github.com>
2025-03-28 23:17:07 -0500

    xd

--- src/main/java/com/project/GUIs/FabricaFrame.java
+++ src/main/java/com/project/GUIs/FabricaFrame.java
@@ -10,32 +10,20 @@ import com.project.Logica.*;
 public class FabricaFrame extends JFrame implements FabricaGUI {
 
     public FabricaFrame() {
-        //this.entrada = new EntradaFrame().a();
-        //this.salida = new SalidaFrame();
         JFrame framePrincipal = new JFrame("Aplicación Principal");
         framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         framePrincipal.setSize(300, 300);
         framePrincipal.setLocationRelativeTo(null);
-        //framePrincipal.add(entrada, BorderLayout.NORTH);
-        //framePrincipal.add(salida, BorderLayout.CENTER);
+
         EntradaFrame entrada = (EntradaFrame) crearEntrada();
         SalidaFrame salida = (SalidaFrame) crearSalida();
 
-        // Agregar los componentes al frame
+
         framePrincipal.add(entrada, BorderLayout.NORTH);
         framePrincipal.add(salida, BorderLayout.CENTER);
 
-        // Crear la lógica y configurar eventos
+
         Logica logica = crearLogica(entrada, salida);
-
-        /*
-        // Crear y agregar los componentes
-        EntradaFrame entradaFrame = (EntradaFrame) crearEntrada();
-        SalidaFrame salidaFrame = (SalidaFrame) crearSalida();
-        
-
-        // Crear lógica (aunque no se agrega al frame)
-        crearLogica(entradaFrame, salidaFrame);*/
 
        framePrincipal.setVisible(true);
     }
